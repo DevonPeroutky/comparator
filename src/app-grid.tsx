7386debf5a4a2b1e36d2c543cd01a27d49b5efd3
Your Name <you@example.com>
2024-10-06 15:32:57 -0700

    Make it all more responsive

--- src/app-grid.tsx
+++ src/app-grid.tsx
@@ -30,27 +30,27 @@ export function AppGrid() {
       description: <div className="my-4" />,
       content: <JobOfferTable />,
       className:
-        "col-span-1 lg:col-span-4 border-b lg:border-r dark:border-neutral-800 xl:pl-32 2xl:pl-44",
+        "col-span-1 md:col-span-2 xl:col-span-4 border-b lg:border-r dark:border-neutral-800 xl:pl-32 2xl:pl-44",
     },
     {
       title: <h2 className="text-4xl font-bold dark:text-white text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Perspective</h2>,
       description:
         <span className="text-lg font-semibold text-gray-500 dark:text-gray-400 leading-8">The world, median per-capita <span className="underline underline-offset-2 decoration-2 decoration-blue-400 dark:decoration-blue-600">household income</span> is only <mark className="px-2 text-white bg-blue-400 rounded dark:bg-blue-600 py-1">$2,920 per year</mark><br /> Making over <mark className="px-2 bg-green-600 rounded py-1 text-white">$124,720 / year</mark> puts you in the <span className="underline underline-offset-2 decoration-2 decoration-green-600 dark:decoration-green-600">top 1% of global earners.</span></span>,
       content: <GlobeCard />,
-      className: "border-b col-span-1 lg:col-span-2 dark:border-neutral-800 xl:pr-32 2xl:pr-44",
+      className: "border-b col-span-1 md:col-span-2 dark:border-neutral-800 xl:pr-32 2xl:pr-44",
     },
     {
       title: <h2 className="text-4xl font-bold dark:text-white">Scenario Builder</h2>,
       description: <ScenarioBuilderDescription />,
       content: <EquityJourney />,
-      className: "col-span-1 lg:col-span-3 border-b lg:border-none xl:pl-32 2xl:pl-44",
+      className: "col-span-1 md:col-span-3 xl:col-span-3 border-b lg:border-none xl:pl-32 2xl:pl-44",
     },
     {
       title: <OfferGraphTitle />,
       description: null,
       content: <OffersGraph />,
       className:
-        "col-span-1 lg:col-span-3 lg:border-l dark:border-neutral-800 xl:pl-32 2xl:pr-44",
+        "col-span-1 md:col-span-2 2xl:col-span-3 lg:border-l dark:border-neutral-800 xl:pl-32 2xl:pr-44",
     },
     {
       title: <h2 className="flex justify-center items-center md:justify-start text-4xl font-bold dark:text-white">Compare Outcomes</h2>,
@@ -61,7 +61,7 @@ export function AppGrid() {
   ];
   return (
     <div className="relative ">
-      <div className="grid grid-cols-1 lg:grid-cols-6 mt-12 border-t rounded-md dark:border-neutral-800">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-6 mt-12 border-t rounded-md dark:border-neutral-800">
         {features.map((feature, idx) => (
           <FeatureCard key={idx} className={feature.className}>
             <FeatureTitle>{feature.title}</FeatureTitle>
