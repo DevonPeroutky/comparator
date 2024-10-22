fa6d93b2a8819435547d13aa9cf4ba1c943a41c8
Devon Peroutky <devonperoutky@Devons-MacBook-Pro.local>
2024-10-22 18:02:14 -0400

    Add lottie animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { AppGrid } from './app-grid';
 import { AnimatedShareButton, ShareButton, TestButton } from './components/app/share-button';
-import { Button } from './components/ui/button';
 import SparklesText from "./components/ui/sparkles-text";
+import Lottie from 'react-lottie-player';
+// import animationData from './assets/animations/test-compare-light.json';
+import animationData from './assets/animations/compare-light.json';
+import { useState } from 'react';
+import { cn } from './lib/utils';
 
 export default function App() {
   const title = "Comparator";
+  const [animationCompleted, setAnimationCompleted] = useState(false);
 
   return (
-    <div className='relative z-20 py-10 lg:py-40 w-screen flex flex-col items-center'>
-      <div className="flex flex-col items-center text-center mx-auto px-10">
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl dark:text-white capitalize">{title}</h1>
-        <div className="flex flex-wrap items-center justify-center mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
+    <div className='relative z-20 pb-10 lg:pb-10 w-screen flex flex-col items-center'>
+      <Lottie animationData={animationData} play loop={false} className="w-[250px] md:w-[400px] lg:w-[500px] xl:w-[600px] 2xl:w-[700px] mb-4" onComplete={() => setAnimationCompleted(true)} />
+      <div className={cn("flex flex-col items-center text-center mx-auto transition-opacity duration-1000", animationCompleted ? "opacity-100" : "opacity-0")}>
+        <div className="flex flex-wrap items-center justify-center mb-4 text-lg font-normal text-gray-500 dark:text-gray-400 " >
           <p>Free (and</p>
           <a href='https://github.com/DevonPeroutky/comparator' target='_blank' className='mx-1 font-medium text-sky-600 dark:text-blue-500 hover:underline'>open-source</a>
           <p>) way of comparing job offers and equity packages. Your data is</p>
@@ -19,15 +24,8 @@ export default function App() {
           <SparklesText text="100% private." sparklesCount={5} duration={5} className="mx-1" />
         </div>
         <ShareButton />
-        {/* <AnimatedShareButton className='mb-10'> */}
-        {/*   <button className='flex items-center w-fit h-10 px-4 py-2 dark:bg-white  bg-white rounded-none outline-none hover:border-transparent'>Copy URL to Share</button> */}
-        {/* </AnimatedShareButton> */}
-        {/* <AnimatedShareButton> */}
-        {/*   <Button className='hover:border-transparent'>Copy URL to Share</Button> */}
-        {/* </AnimatedShareButton> */}
-        {/* <TestButton /> */}
         <AppGrid />
       </div >
-    </div>
+    </div >
   );
 }
